71342345989936cbaa0ed59eb0973a2f6952a94b
humonnom <josojoso0702@gmail.com>
2021-12-27 16:35:46 +0900

    change 07/ex00/whatever.hpp

--- cpp_subjects/07/ex00/whatever.hpp
+++ cpp_subjects/07/ex00/whatever.hpp
@@ -1,8 +1,8 @@
 #ifndef WHATEVER_HPP
-# define WHATEVER_HPP
+#define WHATEVER_HPP
 
-# include <iostream>
-# include <string>
+#include <iostream>
+#include <string>
 
 template <typename ANY>
 void swap(ANY &a, ANY &b) {
@@ -12,28 +12,26 @@ void swap(ANY &a, ANY &b) {
     b = tmp;
 }
 template <typename ANY>
-ANY const& min(ANY &a, ANY &b) {
-    return (a < b? a : b);
+ANY const &min(ANY &a, ANY &b) {
+    return (a < b ? a : b);
 }
-
 template <typename ANY>
-ANY const& max(ANY &a, ANY &b) {
-    return (a > b? a : b);
+ANY const &max(ANY &a, ANY &b) {
+    return (a > b ? a : b);
 }
-
 template <typename ANY>
 void test(ANY x, ANY y) {
-        std::cout << "  x:" << x << std::endl;
-        std::cout << "  y:" << y << std::endl;
-        swap(x, y);
-        std::cout << "  -- swap --" << std::endl;
-        std::cout << "  x:" << x << std::endl;
-        std::cout << "  y:" << y << std::endl;
+    std::cout << "  x:" << x << std::endl;
+    std::cout << "  y:" << y << std::endl;
+    swap(x, y);
+    std::cout << "  -- swap --" << std::endl;
+    std::cout << "  x:" << x << std::endl;
+    std::cout << "  y:" << y << std::endl;
 
-        std::cout << "  -- min --" << std::endl;
-        std::cout << "  " << min(x, y) << std::endl;
-        std::cout << "  -- max --" << std::endl;
-        std::cout << "  " << max(x, y) << std::endl;
+    std::cout << "  -- min --" << std::endl;
+    std::cout << "  " << min(x, y) << std::endl;
+    std::cout << "  -- max --" << std::endl;
+    std::cout << "  " << max(x, y) << std::endl;
 }
 
 #endif
